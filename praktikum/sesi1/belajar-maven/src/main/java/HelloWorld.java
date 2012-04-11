ac209afc3e923b5b9077b579df6e841e0e0ea1ba
Muhammad sidratul Muntaha <tear_ofregret@yahoo.co.id>
2012-04-11 13:38:54 +0700

    commit

--- praktikum/sesi1/belajar-maven/src/main/java/HelloWorld.java
+++ praktikum/sesi1/belajar-maven/src/main/java/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld{
 	public static void main(String[] args){
+		int hasil;
 		System.out.println("Hello java");
+		Kalkulator k= new Kalkulator();
+		hasil=k.tambah(10,5);
+		System.out.println("hasil = "+hasil);
 	}
 }
