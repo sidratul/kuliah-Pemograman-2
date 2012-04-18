9f444220c3b4362519f25549ccc61ff73e0c6cdf
Muhammad sidratul Muntaha <tear_ofregret@yahoo.co.id>
2012-04-18 18:54:51 +0700

    Fixed

--- praktikum/sesi1/Belajar-autotest/src/test/java/HalloTest.java
+++ praktikum/sesi1/Belajar-autotest/src/test/java/HalloTest.java
@@ -1,4 +1,5 @@
 import junit.framework.TestCase;
+import junit.framework.Assert;
 
 public HalloTest extends TestCase{
 	public HalloTest(){
@@ -7,11 +8,14 @@ public HalloTest extends TestCase{
 
 	protected void setUp() throws Exception{
 		super.setUp();
+		System.out.println("hello");
 	}
 
 	protected void tearDown() throws Exception{
 		super.tearDown();
+		System.out.println("World");
 	}
+
 	//TODO add test method here. The name mus begin with 'test'.for example :
 	public void testHallo(){
 		System.out.println("Hello world");
